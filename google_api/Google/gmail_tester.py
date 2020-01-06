e08191c9f9508013a600464251b5b87597020514
charliesusername <iamcharliecohen@gmail.com>
2020-01-06 15:36:08 -0500

    Clearing cache

--- google_api/Google/gmail_tester.py
+++
@@ -1,8 +0,0 @@
-from google_apps import GMail
-
-myGmail = GMail("charles.cohen@nycdatascience.com")
-
-msg = myGmail.create_attached_message("nycdsa.student@gmail.com", "Text File Test",
-                                      "This message contains a text file attachment.\n\nDo not respond.",
-                                      ".", "TODO_2.txt")
-myGmail.send_message(msg)
